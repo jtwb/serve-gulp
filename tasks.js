d511b8596bc14c8eee1619829d5ac01260562ddf
Jason Benterou <jason.benterou@gmail.com>
2015-02-21 13:25:05 -0800

    No minify

--- tasks.js
+++ tasks.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cssmin = require('gulp-minify-css');
 
 
 var tasks = {
   'css': function(src, out) {
     gulp.src(src)
       .pipe(sass())
-      .pipe(cssmin())
       .pipe(out())
   },
 
@@ -17,7 +14,6 @@ var tasks = {
     gulp.src(src)
       .pipe(sass())
       .pipe(concat('all.js'))
-      .pipe(cssmin())
       .pipe(out())
   },
 
@@ -25,27 +21,23 @@ var tasks = {
     gulp.src(src)
       .pipe(sass())
       .pipe(concat('recursive.js'))
-      .pipe(cssmin())
       .pipe(out())
   },
 
   'js': function(src, out) {
     gulp.src(src)
-      .pipe(uglify())
       .pipe(out())
   },
 
   '*js': function(src, out) {
     gulp.src(src)
       .pipe(concat('all.js'))
-      .pipe(uglify())
       .pipe(out())
   },
 
   '**js': function(src, out) {
     gulp.src(src)
       .pipe(concat('recursive.js'))
-      .pipe(uglify())
       .pipe(out())
   }
 };
