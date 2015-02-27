ccb7ca29ad68784a12a23b97408b0cfa4fa368e4
Jason Benterou <jason.benterou@gmail.com>
2015-02-27 11:07:13 -0800

    Add restrictor argv[0]

--- bin/serve-gulp.js
+++ bin/serve-gulp.js
@@ -9,9 +9,11 @@ var server = require('../server/');
 var options = {
   port: process.env.PORT || 80,
   host: process.env.HOST || '0.0.0.0',
+  restrict: argv['restrict'] || argv._[0] || '.',
   basedir: argv['basedir'] || __dirname,
   taskfile: path.resolve(argv['taskfile'] || './tasks.js')
 };
+console.error(options);
 
 server.start(options, function() {
   // runs in HTTP server context
