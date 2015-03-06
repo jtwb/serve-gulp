3d8ea3fd9821a5e279e72c1b0968c42d579133a7
Jason Benterou <jason.benterou@gmail.com>
2015-03-06 00:14:06 -0800

    Use current directory gulpfile.js

--- bin/serve-gulp.js
+++ bin/serve-gulp.js
@@ -3,15 +3,25 @@
 'use strict';
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
+var fs = require('fs');
 var server = require('../server/');
 
+
+var local_gulpfile_found;
+if (!argv['gulpfile']) {
+  try {
+    local_gulpfile_found = fs.statSync(path.resolve('./gulpfile.js'));
+  } catch(e) { }
+}
 
 var options = {
   port: process.env.PORT || 80,
   host: process.env.HOST || '0.0.0.0',
   basedir: path.resolve(argv['basedir'] || '.'),
   restrict: path.resolve(argv['restrict'] || argv._[0] || argv['basedir'] || '.'),
-  gulpfile: path.resolve(argv['gulpfile'] || path.join(__dirname, '../gulpfile.js'))
+  gulpfile: path.resolve(
+      argv['gulpfile'] ||
+      (local_gulpfile_found ? 'gulpfile.js' : path.join(__dirname, '../gulpfile.js')))
 };
 
 if (argv['v'] || argv['verbose']) {
